7dcbcd5348f27a134ea4c3e402a8b345483e3761
luokelly <luokelly@yahoo.com>
2017-03-08 10:46:03 -0800

    Finished Chkpt 1

--- TextExcel/src/textExcel/EmptyCell.java
+++ TextExcel/src/textExcel/EmptyCell.java
@@ -5,13 +5,13 @@ public class EmptyCell implements Cell {
 	@Override
 	public String abbreviatedCellText() {
 		// TODO Auto-generated method stub
-		return "";
+		return "          ";
 	}
 
 	@Override
 	public String fullCellText() {
 		// TODO Auto-generated method stub
-		return "";
+		return "          ";
 	}
 
 }
